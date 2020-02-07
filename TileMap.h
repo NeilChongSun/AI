62c606e8f55996092997add9db3381fb63c9ab78
Neil Sun <chong921130@gmail.com>
2020-02-07 02:43:15 +0000

    Imported from Changeset 206

---
+++ TileMap.h
@@ -0,0 +1,62 @@
+#pragma once
+#include<XEngine.h>
+#include<AI.h>
+enum ESearchMode
+{
+	eBFS,
+	eDFS,
+	eDijkstars,
+	eAStar
+};
+enum EMouseClick
+{
+	eSetStart,
+	eSetEnd,
+	eSetTile
+};
+class TileMap
+{
+public:
+	void Load();
+	void Unload();
+	void Update(float deltaTime);
+	void Render() const;
+
+private:
+	int GetIndex(int x, int y) const;
+	X::Math::Vector2 GetPosition(AI::Coord coord) const;
+	AI::Coord GetClickPosition()const;
+	
+	void ShowDebugUI();
+	
+	std::array<X::TextureId,7> mTextureIds;
+	std::vector<int> mTiles;
+	int mCurrentTile;
+	
+	int mColumns = 0;
+	int mRows = 0;
+	int mTileSize = 32;
+	
+	std::vector<AI::Graph::Node> mNodes;
+	
+	AI::Graph mGraph;
+	AI::Coord mStartPoint = {0,0};
+	AI::Coord mEndPoint = {0,1};
+	
+	X::Math::Vector2 mOffset = { static_cast<float>(mTileSize / 2),static_cast<float>(mTileSize / 2) };
+	
+	std::vector<AI::Coord> mPath;
+	std::list<AI::Coord> mCloseList;
+	std::vector<AI::Coord> mParents;
+
+	AI::BFS mBFS;
+	AI::DFS mDFS;
+
+	std::string mSearchModeText="";
+	std::string mCurrentClickText="";
+
+	ESearchMode mSearchMode;
+	EMouseClick mMouseClick;
+
+	bool mDraw = false;
+};
